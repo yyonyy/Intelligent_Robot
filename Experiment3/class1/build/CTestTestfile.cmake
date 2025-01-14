046c09a684567a25a503b5bbdd1a5676eb8c11b1
yyonyy <yw8431@g.skku.edu>
2025-01-14 14:41:33 +0900

    upload

---
+++ Experiment3/class1/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sa/RE510_2024/Experiment3/class1/src
+# Build directory: /home/sa/RE510_2024/Experiment3/class1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("tutorial")
